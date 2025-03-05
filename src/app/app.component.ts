bf53731916314fa836015fe17e22d67a3933a673
Adeel Khan <adeelbarki@gmail.com>
2025-03-04 20:35:16 -0330

    added dashboard design cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
 import { Router } from '@angular/router';
-import { NgIf } from '@angular/common';
+// import { NgIf } from '@angular/common';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { Amplify } from 'aws-amplify';
 import { environment } from '../environments/environment';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, AmplifyAuthenticatorModule],
+  imports: [AmplifyAuthenticatorModule, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -21,16 +22,5 @@ export class AppComponent {
     Amplify.configure(environment.amplifyConfig);
   }
 
-  async signOut() {
-    
-  }
 
-  async logout() {
-    // await this.authService.logout();
-    this.router.navigate(['/auth/login']); // Redirect to login page after logout
-  }
-
-  get isAuthenticated(): boolean {
-    return this.authService.isAuthenticated();
-  }
 }
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { NgIf } from '@angular/common';
@@ -6,26 +6,11 @@ import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgIf],
+  // imports: [NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  user: any = null;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  async ngOnInit() {
-    try {
-      this.user = await this.authService.getCurrentUser();
-    } catch (error) {
-      console.error('User not authenticated:', error);
-      this.router.navigate(['/auth/login']); // ✅ Redirect to login if not authenticated
-    }
-  }
-
-  async logout() {
-    await this.authService.logout();
-    this.router.navigate(['/auth/login']); // ✅ Redirect to login after logout
-  }
+  
+  @Input() signOut!: () => void;
 }
