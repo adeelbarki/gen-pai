2794f930118ee6c04be02155bd5c672b40fd7fd9
Adeel Khan <adeelbarki@gmail.com>
2025-03-05 12:38:15 -0330

    added dashboard frontend design and tabs

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,16 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { ChatSidebarComponent } from './ChatSidebarComponent/chat-sidebar.component';
+import { ChatWindowComponent } from './ChatWindowComponent/chat-window.component';
+import { MedicalTimelineComponent } from './MedicalTimelineComponent/medical-timeline.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  // imports: [NgIf],
+  imports: [
+    ChatSidebarComponent,
+    ChatWindowComponent,
+    MedicalTimelineComponent,
+    CommonModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  
   @Input() signOut!: () => void;
+  activeTab: string = 'Dashboard';
+
+  tabs = [
+    { name: 'Dashboard', label: 'Dashboard' },
+    { name: 'Studies', label: 'Studies' },
+    { name: 'History', label: 'History' },
+    { name: 'Profile', label: 'Profile' },
+    { name: 'Help', label: 'Help' },
+  ];
+
+  setActiveTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
