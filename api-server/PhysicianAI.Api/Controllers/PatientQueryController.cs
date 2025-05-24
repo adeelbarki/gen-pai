98cc8fd33c080a41c1c9f28cc5e616914d31aa1e
Adeel Khan <adeelbarki@gmail.com>
2025-05-24 10:54:39 -0230

    added basic dotnet server for api routes

---
+++ api-server/PhysicianAI.Api/Controllers/PatientQueryController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhysicianAI.Api.Controllers
+{
+    [ApiController]
+    [Route("api/patient/query")]
+    public class PatientQueryController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult QueryPatient([FromBody] UserQuestion question)
+        {
+            return Ok($"Received question: {question.Text}");
+        }
+    }
+    public class UserQuestion
+    {
+        public string Text { get; set; }
+    }
+}
